1c7c65390bfc2e1eef05262281a0efa467d95dba
Rajalakshmi67 <rrajalakshmi1102@gmail.com>
2025-04-25 14:34:24 +0530

    update

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.VITE_API_URL || 'http://localhost:5000/api/auth';
+const API_URL = import.meta.env.VITE_RENDER_API_URL || 'https://cinebuzz-d2yp.onrender.com/api/auth';
 
 export interface UserData {
   name?: string;
